10f6a11b2aefd30dc002d66730758b0b09a748c6
Kevin Wolbart <kevinwolbart@gmail.com>
2022-10-19 09:39:32 -0500

    Working calculator contains minor bugs

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const display = document.querySelector(".display");
+let operations = {
+    number: "",
+    operator: "",
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -17,17 +23,60 @@ function divide(a, b) {
 function operate(operator, a, b) {
     switch(operator) {
         case "+":
-            add(a,b);
+            return add(a,b);
             break;
         case "-":
-            subtract(a,b);
+            return subtract(a,b);
             break;
         case "*":
-            multiply(a,b);
+            return multiply(a,b);
             break;
         case "/":
-            divide(a,b);
+            return divide(a,b);
             break;
     } 
 }
 
+function displayNumber(num) {
+    if (display.textContent === "Improper Entry, Retry") {
+        display.textContent = "";
+    } else if (display.textContent.includes("=")) {
+        display.textContent = num;
+    } else {
+        display.textContent += num;
+    }
+}
+
+function calculate() {
+    if (!operations.operator || !operations.number || !display.textContent) {
+        display.textContent = "Improper Entry, Retry";
+        operations.number = "";
+        operations.operator = "";
+    } else {
+        display.textContent = `= ${operate(operations.operator, operations.number, Number(display.textContent))}`;
+    }
+}
+
+function enterOperator(op) {
+    if (!operations.operator && !operations.number) {
+        operations.operator = op;
+        operations.number = Number(display.textContent);
+        console.log(operations);
+        display.textContent = "";
+    } else if (operations.operator && !display.textContent) {
+        display.textContent = "Improper Entry, Retry";
+        operations.number = "";
+        operations.operator = "";
+    } else if (operations.operator && operations.number) {
+        operations.number = operate(operations.operator, operations.number, Number(display.textContent));
+        operations.operator = op;
+        console.log(operations);
+        display.textContent = `= ${operations.number}`;
+    }
+}
+
+function clear() {
+    display.textContent = "";
+    operations.number = "";
+    operations.operator = "";
+}
